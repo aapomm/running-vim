689f48714d97d7008df7d0f101284044e9c7cc10
Aaron Manaloto <aaronpomanaloto@gmail.com>
2015-05-02 13:07:18 +0800

    redo game loop

--- js/game.js
+++ js/game.js
@@ -1,19 +1,30 @@
-(function(game){
-  var $lines = [
-    $('.line1'),
-    $('.line2'),
-    $('.line3'),
-    $('.line4'),
-    $('.line5')
-  ]
+function Game($lines){
+  this.LINE_NUM = 5;
+  this.MAX_CHARACTERS = 40;
 
-  game.run = function(){
+  // In milliseconds
+  this.updateFrequency = 500;
 
-    return function(){
-      for(var i=0; i<5; i++) {
-        $lines[i].append('&nbsp;');
-      }
+  this.$lines = $lines;
+}
+
+Game.prototype.limitLineLength = function(){
+  for(var i=0; i < this.LINE_NUM; i++) {
+    var line = this.$lines[i]
+    if (line.find('.letter').length == this.MAX_CHARACTERS){
+      line.find('.letter:first').remove();
     }
   }
+}
 
-})(window.Game = {});
+Game.prototype.update = function(){
+
+}
+
+Game.prototype.draw = function(){
+  this.limitLineLength();
+
+  for(var i=0; i < this.LINE_NUM; i++) {
+    this.$lines[i].append("<span class='letter'>&nbsp;</span>");
+  }
+}
--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
-(function(game){
-  setInterval(game.run(), 500);
-})(window.Game);
+$(function(){
+  // DOM objects
+  var $lines = [
+    $('.line1'),
+    $('.line2'),
+    $('.line3'),
+    $('.line4'),
+    $('.line5')
+  ]
+
+  // Instantiate Game class
+  var game = new Game($lines);
+
+  var timestamp = $.now();
+
+  // Game loop
+  // Updates every 0.01 of a second
+
+  setInterval(function(){
+    var timenow = $.now();
+    if (timenow > timestamp + game.updateFrequency) {
+      game.update();
+      game.draw();
+
+      timestamp = $.now();
+    }
+  }, 10);
+});
